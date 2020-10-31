9922fc4019c8f8dfd5a76950cfb0e468d79841d2
Sławomir Paszko <folkus@gmail.com>
2020-11-01 00:34:07 +0100

    Training start fix.

--- scripts/training-start.sh
+++ scripts/training-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Starting training pre-configured yolov4!"
-./darknet detector train data/obj.data yolo-obj.cfg yolov4.conv.137
+./darknet detector train build/darknet/x64/data/obj.data cfg/yolo-obj.cfg yolov4.conv.137
