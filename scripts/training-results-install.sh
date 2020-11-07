395500cf54a319b8fec3ca4326dfb5c285ab2a4e
Sławomir Paszko <folkus@gmail.com>
2020-11-07 22:34:45 +0100

    Fix.

--- scripts/training-results-install.sh
+++ scripts/training-results-install.sh
@@ -9,8 +9,8 @@ sufix="${1}"
 output="../example1-yolo/ObjectDetectors/yolov4${sufix}"
 mkdir -p ${output}/cfg
 
-cp -rfv build/darknet/x64/cfg/yolo-obj.cfg ${output}/cfg/yolov4.cfg
-cp -rfv build/darknet/x64/backup/yolo-obj_last.weights ${output}/cfg/yolov4.weights
-cp -rfv build/darknet/x64/data/obj.data ${output}/cfg/${sufix}.data
-cp -rfv build/darknet/x64/data/obj.names ${output}/cfg/${sufix}.names
+cp -Hrfv build/darknet/x64/cfg/yolo-obj.cfg ${output}/cfg/yolov4.cfg
+cp -Hrfv build/darknet/x64/backup/yolo-obj_last.weights ${output}/cfg/yolov4.weights
+cp -Hrfv build/darknet/x64/data/obj.data ${output}/cfg/${sufix}.data
+cp -Hrfv build/darknet/x64/data/obj.names ${output}/cfg/${sufix}.names
 
