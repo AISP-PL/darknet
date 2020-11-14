85e685858b7ca7a44ee3f5d14a1414ee28d45e52
Sławomir Paszko <folkus@gmail.com>
2020-11-14 17:03:45 +0100

    Fixes.

--- build/darknet/x64/training-test.sh
+++ build/darknet/x64/training-test.sh
@@ -3,8 +3,8 @@ imagename=$(basename ${imagepath})
 outimagename="test.${imagename}"
 cp ${imagepath} ${outimagename}
 
-convert ${outimagename} -resize 1280\> output.png
-darknet detector test data/obj.data cfg/yolo-obj.cfg backup/yolo-obj_last.weights output.png
+convert ${outimagename} -resize 1280\> ${outimagename}
+darknet detector test data/obj.data cfg/yolo-obj.cfg backup/yolo-obj_last.weights ${outimagename}
 # clean
-rm -rfv output.png ${outimagename}
+rm -rfv ${outimagename}
 
