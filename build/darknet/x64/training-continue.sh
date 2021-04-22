d1ea27c5e622aa140c6f9add084c3a11b2b54602
Sławomir Paszko <folkus@gmail.com>
2021-04-22 10:34:17 +0200

    Fixes in training scripts.

--- build/darknet/x64/training-continue.sh
+++ build/darknet/x64/training-continue.sh
@@ -1,1 +1,19 @@
-darknet detector train data/obj.data cfg/yolo-obj.cfg backup/yolo-obj_last.weights -map ${@}
+# Get number of GPU's
+gpus=$(lspci | grep VGA.*NVIDIA | wc -l)
+if [ ${gpus} -eq 0 ]; then
+    echo "Error! Missing GPU!"
+    exit -1
+elif [ ${gpus} -eq 1 ]; then 
+    gpuArg="0"
+elif [ ${gpus} -eq 2 ]; then 
+    gpuArg="0,1"
+elif [ ${gpus} -eq 3 ]; then 
+    gpuArg="0,1,2"
+elif [ ${gpus} -eq 4 ]; then 
+    gpuArg="0,1,2,3"
+fi
+
+echo "Found ${gpus}!"
+echo "GPU argument is ${gpuArg}"
+sleep 1
+darknet detector train data/obj.data cfg/yolo-obj.cfg backup/yolo-obj_last.weights -map ${@} -gpus ${gpuArg}
--- build/darknet/x64/training-start.sh
+++ build/darknet/x64/training-start.sh
@@ -1,1 +1,7 @@
+
+echo "Clear backup!"
+rm -rfv backup.old/
+mv backup backup.old
+
+echo "Start training!"
 darknet detector train data/obj.data cfg/yolo-obj.cfg yolov4.conv.137 -map
