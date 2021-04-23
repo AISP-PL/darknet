763ad476ac8fe8afc543a71de7d0a91440423967
Sławomir Paszko <folkus@gmail.com>
2021-04-23 16:04:57 +0200

    New fixes.

--- build/darknet/x64/training-start.sh
+++ build/darknet/x64/training-start.sh
@@ -1,7 +1,29 @@
-
 echo "Clear backup!"
 rm -rfv backup.old/
 mv backup backup.old
 
+choice=$(dialog --clear --backtitle "Select" \
+       --title "YOLO model to training" --menu "Choose one of the following models:" \
+       15 40 4 \
+1 "YOLOv4" \
+2 "YOLOv4-CSP" 3>&2 2>&1 1>&3)
+
+clear
+case ${choice} in
+        1)
+            model="yolov4.conv.137"
+            modelcfg="cfg/yolo-obj.cfg"
+            ;;
+        2)
+            model="yolov4-csp.conv.142"
+            modelcfg="cfg/yolov4-csp.cfg"
+            ;;
+        *)
+            echo "Error! Unknown ${choice}!"
+            exit -1
+            ;;
+esac
+
 echo "Start training!"
-darknet detector train data/obj.data cfg/yolo-obj.cfg yolov4.conv.137 -map
+echo "Training with ${model}, ${modelcfg}."
+darknet detector train data/obj.data ${modelcfg} ${model} -map
