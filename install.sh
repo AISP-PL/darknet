d93f51a3745e31323d0afba35b95b95fee9994c7
Sławomir Paszko <folkus@gmail.com>
2021-07-15 14:25:21 +0200

    Added libdarknet.so install.

--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@ cmake ..
 cmake --build . --target install --parallel 8
 
 sudo ln -sf /home/spasz/python/pyAITracker/darknet/darknet /usr/bin/darknet
+
+# Install libraries
+sudo cp libdarknet.so /usr/local/lib/
+sudo cp include/darknet.h /usr/local/include/
+sudo ldconfig
