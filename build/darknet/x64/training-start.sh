9533f0104aec529c051762a23e96c7be36b431ec
Sławomir Paszko <folkus@gmail.com>
2021-09-04 11:45:17 +0200

    Fix.

--- build/darknet/x64/training-start.sh
+++ build/darknet/x64/training-start.sh
@@ -6,19 +6,26 @@ mkdir -p backup
 choice=$(dialog --clear --backtitle "Select" \
        --title "YOLO model to training" --menu "Choose one of the following models:" \
        15 40 4 \
-1 "YOLOv4" \
-2 "YOLOv4-CSP" 3>&2 2>&1 1>&3)
+1 "YOLOv4-GDDKIA" \
+2 "YOLOv4-plates" \
+3 "YOLOv4-GDDKIA-CSP" 3>&2 2>&1 1>&3)
 
 clear
 case ${choice} in
         1)
             model="yolov4.conv.137"
-            modelcfg="cfg/yolo-obj.cfg"
+            datacfg="cfg/yolo-gddkia.data"
+            modelcfg="cfg/yolo-gddkia.cfg"
             ;;
         2)
-            model="yolov4-csp.conv.142"
-            modelcfg="cfg/yolov4-csp.cfg"
+            model="yolov4-tiny.weights"
+            datacfg="cfg/yolo-plates.data"
+            modelcfg="cfg/yolo-plates.cfg"
             ;;
+#       3)
+#            model="yolov4-csp.conv.142"
+#            modelcfg="cfg/yolov4-csp.cfg"
+#            ;;
         *)
             echo "Error! Unknown ${choice}!"
             exit -1
@@ -27,4 +34,4 @@ esac
 
 echo "Start training!"
 echo "Training with ${model}, ${modelcfg}."
-darknet detector train data/obj.data ${modelcfg} ${model} -map
+darknet detector train ${datacfg} ${modelcfg} ${model} -map
