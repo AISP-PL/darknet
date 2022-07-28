7ac37f51d3afad84c7c3e1ac12303b83618c81eb
Sławomir Paszko <folkus@gmail.com>
2022-07-28 22:36:09 +0200

    Fix.

--- build/darknet/x64/training-map.sh
+++ build/darknet/x64/training-map.sh
@@ -1,4 +1,5 @@
 # Get name of trained YOLO
 yoloname=$(ls backup/*last.weights | cut -c 8- | rev | cut -c 14- | rev)
 echo "Found trained YOLO ${yoloname}!"
+echo "darknet detector map cfg/${yoloname}.data cfg/${yoloname}.cfg backup/${yoloname}_best.weights -gpus 0,1"
 darknet detector map cfg/${yoloname}.data cfg/${yoloname}.cfg backup/${yoloname}_best.weights -gpus 0,1
