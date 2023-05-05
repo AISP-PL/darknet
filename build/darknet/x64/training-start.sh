6735c58db6e1e7773883cae4fd663fd358702d10
Sławomir Paszko <folkus@gmail.com>
2023-05-05 11:04:52 +0200

    Added ExtGddkia v7 version.

--- build/darknet/x64/training-start.sh
+++ build/darknet/x64/training-start.sh
@@ -8,10 +8,11 @@ choice=$(dialog --clear --backtitle "Select" \
        15 40 5 \
 1 "YOLOv4-GDDKIA" \
 2 "YOLOv4-ExtGDDKIA" \
-3 "YOLOv4-ExtGDDKIA-lite" \
-4 "YOLOv4-ALPROCR" \
-5 "YOLOv4-ExtGDDKIA-CSP" \
-6 "YOLOv4-VehliceFeatures" 3>&2 2>&1 1>&3)
+3 "YOLOv7-ExtGDDKIA (v7)" \
+4 "YOLOv4-ExtGDDKIA-lite" \
+5 "YOLOv4-ALPROCR" \
+6 "YOLOv4-ExtGDDKIA-CSP" \
+7 "YOLOv4-VehliceFeatures" 3>&2 2>&1 1>&3)
 
 clear
 case ${choice} in
@@ -26,21 +27,26 @@ case ${choice} in
             modelcfg="cfg/yolo-extgddkia.cfg"
             ;;
         3)
+            model="yolov7.conv.132"
+            datacfg="cfg/yolov7-extgddkia.data"
+            modelcfg="cfg/yolov7-extgddkia.cfg"
+            ;;
+        4)
             model="yolov4-tiny.conv.29"
             datacfg="cfg/yolo-extgddkialite.data"
             modelcfg="cfg/yolo-extgddkialite.cfg"
             ;;
-        4)
+        5)
             model="yolov4-tiny.conv.29"
             datacfg="cfg/yolo-alpr.data"
             modelcfg="cfg/yolo-alpr.cfg"
             ;;
-        5)
+        6)
             model="yolov4-csp.conv.142"
             datacfg="cfg/yolo-extgddkia-csp.data"
             modelcfg="cfg/yolo-extgddkia-csp.cfg"
             ;;
-        6)
+        7)
             model="yolov4-tiny.conv.29"
             datacfg="cfg/yolo-vehlicefeatures.data"
             modelcfg="cfg/yolo-vehlicefeatures.cfg"
