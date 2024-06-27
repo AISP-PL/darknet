42c473fa6a20e986ee9061672d45d5fd09f8c35a
Sławomir Paszko <slawomir.paszko@aiut.com>
2024-06-27 10:08:25 +0200

    CPU install added.

---
+++ install-cpu.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Make sure the Makefile exists
+if [[ ! -f Makefile ]]; then
+    echo "Makefile not found!"
+    exit 1
+fi
+
+# Use sed to replace the necessary lines
+sed -i 's/GPU=1/GPU=0/' Makefile
+sed -i 's/CUDNN=1/CUDNN=0/' Makefile
+sed -i 's/CUDNN_HALF=1/CUDNN_HALF=0/' Makefile
+sed -i 's/AVX=0/AVX=1/' Makefile
+sed -i 's/OPENMP=0/OPENMP=1/' Makefile
+
+echo "Makefile updated for CPU+AVX+OPENMP compilation without CUDA."
+
+
+#mkdir build_release
+#cd build_release
+#cmake ..
+#cmake --build . --target install --parallel 8
+make -j9 clean
+make -j9
+echo "Compiled."
+
+
+# Install libraries
+sudo ln -sf $(pwd)/darknet /usr/bin/darknet
+sudo cp -rfv libdarknet.so /usr/local/lib/
+sudo cp -rfv include/darknet.h /usr/local/include/
+sudo ldconfig
+echo "Installed in system."
